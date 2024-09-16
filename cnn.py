5ec5ed2c75a661da0a023bc98d3b8d1a5d6a82de
me_me <me_me@archlinux>
2024-09-17 00:34:58 +0300

    created cnn arch file

---
+++ cnn.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class CNN(torch.nn.Module):
+    def __init__(self)->None:
+        super(CNN, self).__init__()
+
+
+    def forward(self, X:torch.Tensor)->torch.Tensor:
+        pass
+
+    
